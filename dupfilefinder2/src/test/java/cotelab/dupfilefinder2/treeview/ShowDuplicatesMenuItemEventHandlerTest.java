73b4d06119db15087328f8b25b7e6559325e4fb0
Alan T. Coté <alantcote@gmail.com>
2022-01-18 09:23:28 -0500

    Now have test cases for all project application classes. Prepare the ticker-tape parade!

---
+++ dupfilefinder2/src/test/java/cotelab/dupfilefinder2/treeview/ShowDuplicatesMenuItemEventHandlerTest.java
@@ -0,0 +1,142 @@
+package cotelab.dupfilefinder2.treeview;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.Sequence;
+import org.jmock.imposters.ByteBuddyClassImposteriser;
+import org.jmock.lib.concurrent.Synchroniser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cotelab.dupfilefinder2.FXMLController;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
+import net.sf.cotelab.jfxrunner.JavaFxJUnit4ClassRunner;
+
+/**
+ * Test case for
+ * {@link cotelab.dupfilefinder2.treeview.ShowDuplicatesMenuItemEventHandler}.
+ */
+@RunWith(JavaFxJUnit4ClassRunner.class)
+public class ShowDuplicatesMenuItemEventHandlerTest {
+	protected Mockery context;
+	protected Sequence sequence;
+
+	@After
+	public void runAfterTests() throws Exception {
+		context.assertIsSatisfied();
+	}
+
+	@Before
+	public void runBeforeTests() throws Exception {
+		context = new Mockery() {
+			{
+				setThreadingPolicy(new Synchroniser());
+				setImposteriser(ByteBuddyClassImposteriser.INSTANCE);
+			}
+		};
+
+		sequence = context.sequence(getClass().getName());
+	}
+
+	/**
+	 * Test method for
+	 * {@link cotelab.dupfilefinder2.treeview.ShowDuplicatesMenuItemEventHandler#handle(javafx.event.ActionEvent)}.
+	 */
+	@Test
+	public void testHandle() {
+		final Path mockPath = context.mock(Path.class, "mockPath");
+		@SuppressWarnings("unchecked")
+		final Map<Path, Collection<Path>> mockMap = context.mock(Map.class, "mockMap");
+		@SuppressWarnings("unchecked")
+		final Collection<Collection<Path>> mockCollection = context.mock(Collection.class, "mockCollection");
+		final FXMLController mockFXMLController = context.mock(FXMLController.class, "mockFXMLController");
+		final SimpleIntegerProperty sdcCount = new SimpleIntegerProperty(0);
+		ShowDuplicatesMenuItemEventHandler fixture = new ShowDuplicatesMenuItemEventHandler(mockPath, mockMap,
+				mockCollection, mockFXMLController) {
+
+			@Override
+			protected void showDupCollection(Path path) {
+				sdcCount.set(1 + sdcCount.get());
+			}
+
+		};
+		final ActionEvent mockActionEvent = context.mock(ActionEvent.class, "mockActionEvent");
+
+		fixture.handle(mockActionEvent);
+
+		assertEquals(1, sdcCount.get());
+	}
+
+	/**
+	 * Test method for
+	 * {@link cotelab.dupfilefinder2.treeview.ShowDuplicatesMenuItemEventHandler#newDupItemsDialog()}.
+	 */
+	@Test
+	public void testNewDupItemsDialog() {
+		final Path mockPath = context.mock(Path.class, "mockPath");
+		@SuppressWarnings("unchecked")
+		final Map<Path, Collection<Path>> mockMap = context.mock(Map.class, "mockMap");
+		@SuppressWarnings("unchecked")
+		final Collection<Collection<Path>> mockCollection = context.mock(Collection.class, "mockCollection");
+		final FXMLController mockFXMLController = context.mock(FXMLController.class, "mockFXMLController");
+		ShowDuplicatesMenuItemEventHandler fixture = new ShowDuplicatesMenuItemEventHandler(mockPath, mockMap,
+				mockCollection, mockFXMLController);
+		final Collection<Path> realCollection = new ArrayList<Path>();
+
+		context.checking(new Expectations() {
+			{
+				oneOf(mockMap).get(mockPath);
+				will(returnValue(realCollection));
+
+				oneOf(mockMap).get(mockPath);
+				will(returnValue(realCollection));
+			}
+		});
+
+		assertNotNull(fixture.newDupItemsDialog());
+	}
+
+	/**
+	 * Test method for
+	 * {@link cotelab.dupfilefinder2.treeview.ShowDuplicatesMenuItemEventHandler#showDupCollection(java.nio.file.Path)}.
+	 */
+	@Test
+	public void testShowDupCollection() {
+		// I know of no way to intercept a call to a final method.
+	}
+
+	/**
+	 * Test method for
+	 * {@link cotelab.dupfilefinder2.treeview.ShowDuplicatesMenuItemEventHandler#ShowDuplicatesMenuItemEventHandler(java.nio.file.Path, java.util.Map, java.util.Collection, cotelab.dupfilefinder2.FXMLController)}.
+	 */
+	@Test
+	public void testShowDuplicatesMenuItemEventHandler() {
+		final Path mockPath = context.mock(Path.class, "mockPath");
+		@SuppressWarnings("unchecked")
+		final Map<Path, Collection<Path>> mockMap = context.mock(Map.class, "mockMap");
+		@SuppressWarnings("unchecked")
+		final Collection<Collection<Path>> mockCollection = context.mock(Collection.class, "mockCollection");
+		final FXMLController mockFXMLController = context.mock(FXMLController.class, "mockFXMLController");
+		ShowDuplicatesMenuItemEventHandler fixture = new ShowDuplicatesMenuItemEventHandler(mockPath, mockMap,
+				mockCollection, mockFXMLController);
+
+		assertNotNull(fixture);
+
+		assertEquals(mockPath, fixture.path);
+		assertEquals(mockMap, fixture.pathToDupCollMap);
+		assertEquals(mockCollection, fixture.dupCollections);
+		assertEquals(mockFXMLController, fixture.controller);
+	}
+
+}
